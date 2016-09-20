44e95115b756c2b93379ff576ceb89f79c776181
Raghu Betina <rbetina@gmail.com>
2016-09-19 22:55:51 -0500

    Direct association between photos and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920035545) do
+ActiveRecord::Schema.define(version: 20160920035548) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160920035545) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "likes_count"
+    t.integer  "comments_count"
   end
 
   create_table "users", force: :cascade do |t|
