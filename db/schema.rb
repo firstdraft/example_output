1247424fd0e7f0572d0bd63c75d41738d52b3868
Raghu Betina <rbetina@gmail.com>
2016-09-19 22:55:38 -0500

    Direct association between users and friend_requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920035533) do
+ActiveRecord::Schema.define(version: 20160920035536) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,12 +38,12 @@ ActiveRecord::Schema.define(version: 20160920035533) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                      default: "", null: false
-    t.string   "encrypted_password",         default: "", null: false
+    t.string   "email",                          default: "", null: false
+    t.string   "encrypted_password",             default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              default: 0,  null: false
+    t.integer  "sign_in_count",                  default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -53,9 +53,10 @@ ActiveRecord::Schema.define(version: 20160920035533) do
     t.string   "avatar"
     t.string   "phone_number"
     t.string   "website"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.integer  "sent_friend_requests_count"
+    t.integer  "received_friend_requests_count"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
