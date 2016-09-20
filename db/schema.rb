a9871e4a2d85c7405fd4c09845dbfc044ceeeece
Raghu Betina <rbetina@gmail.com>
2016-09-19 22:55:31 -0500

    Generate comments resource with starter:resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920035525) do
+ActiveRecord::Schema.define(version: 20160920035528) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer  "sender_id"
