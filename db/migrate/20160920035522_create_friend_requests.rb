061b6e97078bad9e79ff44b7523a3051da7e22b8
Raghu Betina <rbetina@gmail.com>
2016-09-21 08:57:25 -0500

    Add default value to column

--- db/migrate/20160920035522_create_friend_requests.rb
+++ db/migrate/20160920035522_create_friend_requests.rb
@@ -3,7 +3,7 @@ class CreateFriendRequests < ActiveRecord::Migration
     create_table :friend_requests do |t|
       t.integer :sender_id
       t.integer :recipient_id
-      t.string :status
+      t.string :status, default: "pending"
 
       t.timestamps
 
